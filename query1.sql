7676b72d4f2525fed33cf061a5de945bf954cb1a
VishalBagwe <vishalbagwe171@gmail.com>
2023-06-11 15:47:39 +0530

    Query for Total number of orders placed by each customer

--- query1.sql
+++ query1.sql
@@ -1,4 +1,4 @@
-/* Query to Get the total number of orders placed by each customer */
-SELECT customer_id, COUNT(*) AS total_orders
+/*Query for Total number of orders placed by each customer*/
+SELECT customer_id, COUNT(order_id) AS total_orders
 FROM orders
 GROUP BY customer_id;
