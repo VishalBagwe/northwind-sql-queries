6367ae1c19fa560ce2e1e72967ab429993a5cfed
VishalBagwe <vishalbagwe171@gmail.com>
2023-06-12 14:01:11 +0530

    Get the list of customers who have ordered ‘Chai’ product

--- query21.sql
+++ query21.sql
@@ -1,7 +1,9 @@
-/*Get the list of customers who have ordered ‘Chai’ product*/
-SELECT c.customer_id, c.company_name
-FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN order_details od ON o.order_id = od.order_id
-JOIN products p ON od.product_id = p.product_id
-WHERE p.product_name = 'Chai';
+/*Get the list of customers who have ordered ‘Chai’ product */
+
+select c.customer_id,c.company_name ,p.product_name from customers c inner join orders o
+On c.customer_id = o.customer_id
+inner join order_details o_d 
+On o.order_id = o_d.order_id
+Inner join products p 
+On o_d.product_id = p.product_id
+where product_name = "Chai";
