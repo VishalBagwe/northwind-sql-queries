5e0239c9beb2adcf97742d67c269d0bd13093f54
VishalBagwe <vishalbagwe171@gmail.com>
2023-06-12 11:37:27 +0530

    Get the employees who have processed orders for ‘Chai’ product

---
+++ query22.sql
@@ -0,0 +1,8 @@
+/*Get the employees who have processed orders for ‘Chai’ product*/
+
+SELECT DISTINCT e.employee_id, e.first_name,e.last_name
+FROM employees e
+JOIN orders o ON e.employee_id = o.employee_id
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+WHERE p.product_name = 'Chai';
